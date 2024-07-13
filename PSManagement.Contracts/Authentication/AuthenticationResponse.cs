5e798685f831dec72cf5c0e4f20907d8c80c16b6
hasan khaddour <hasan.b.khaddour@gmail.com>
2024-07-13 18:05:58 +0300

    Rename Project Name.

---
+++ PSManagement.Contracts/Authentication/AuthenticationResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSManagement.Contracts.Authentication
+{
+    public record AuthenticationResponse(
+        Guid Id ,
+        String FirstName,
+        String LastName ,
+        String Email ,
+        String Token
+        );
+}
---
+++ PSManagement.Contracts/Authentication/RegisterRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSManagement.Contracts.Authentication
+{
+    public record RegisterRequest (
+        String FirstName , 
+        String LastName ,
+        String Email,
+        String Password
+        );
+}
