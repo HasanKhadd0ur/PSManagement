d9535ed176cb5bf0b947f133ef1f7bb905008211
hasan khaddour <hasan.b.khaddour@gmail.com>
2024-07-16 16:20:36 +0300

    fix s.

---
+++ PSManagement.Presentation/DI/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PSManagement.Presentaion.DI
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPresentation(this IServiceCollection services)
+        {
+
+            return services;
+        }
+
+
+    }
+}
