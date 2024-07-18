85f8d4fdb0eb53d61b60f67485f68806a0a17f45
hasan khaddour <hasan.b.khaddour@gmail.com>
2024-07-18 11:42:30 +0300

    add Domain Error interface .

---
+++ PSManagement.SharedKernel/DomainErrors/IDomainError.cs
@@ -0,0 +1,14 @@
+﻿using FluentResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSManagement.SharedKernel.DomainErrors
+{
+    public interface IDomainError : IError
+    {
+
+    }
+}
