6795550bd58b09ed0e4e418faa8bf2473fff4a7d
hasan khaddour <hasan.b.khaddour@gmail.com>
2024-07-26 13:36:38 +0300

    Add Project Domain

---
+++ PSManagement.Domain/Projects/Repositories/IProjectRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSManagement.Domain.Projects.Repositories
+{
+    public interface IProjectRepository
+    {
+    }
+}
