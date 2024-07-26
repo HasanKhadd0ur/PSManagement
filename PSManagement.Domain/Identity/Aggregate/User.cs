cb51e1b6813e74518eebfa9abab07e813bbc73b6
hasan khaddour <hasan.b.khaddour@gmail.com>
2024-07-26 13:36:26 +0300

    Add identity Domain

---
+++ PSManagement.Domain/Identity/Aggregate/User.cs
@@ -0,0 +1,15 @@
+﻿using PSManagement.SharedKernel.Aggregate;
+using PSManagement.SharedKernel.Entities;
+using System;
+
+namespace PSManagement.Domain.Identity.Aggregate
+{
+    public class User : IAggregateRoot
+    {
+        public string Email { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string HashedPassword { get; set; }
+
+    }
+}
---
+++ PSManagement.Domain/Identity/Repositories/IUsersRepository.cs
@@ -0,0 +1,16 @@
+﻿using PSManagement.Domain.Identity.Aggregate;
+using PSManagement.SharedKernel.Interfaces;
+using PSManagement.SharedKernel.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSManagement.Domain.Identity.Repositories
+{
+    public interface IUsersRepository : IRepository<User>
+    {
+        public Task<User> GetByEmail(string email, ISpecification<User> specification = null);
+    }
+}
